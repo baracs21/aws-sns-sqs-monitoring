113a141d7b894d448616cf11847a1467232ad8b9
Simon Beddig <simon.beddig@googlemail.com>
2022-06-19 11:06:12 +0200

    update custom resource

--- infrastructure/lib/custom-resources-stack.ts
+++ infrastructure/lib/custom-resources-stack.ts
@@ -2,18 +2,22 @@ import {Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {Code, Function, Runtime} from 'aws-cdk-lib/aws-lambda';
 import {RetentionDays} from "aws-cdk-lib/aws-logs";
+import {ManagedPolicy, Policy, Role} from 'aws-cdk-lib/aws-iam';
 
 export class CustomResourcesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Function(this, 'extended-topic', {
+    const extendTopicFunction = new Function(this, 'extended-topic', {
       functionName: 'ExtendedTopic',
       runtime: Runtime.GO_1_X,
       handler: 'topic-monitoring',
       code: Code.fromAsset('../src/.build/topic-monitoring.zip'),
       logRetention: RetentionDays.ONE_DAY,
-      memorySize: 128
+      memorySize: 128,
     })
+
+    extendTopicFunction.role?.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSNSFullAccess'))
+
   }
 }
--- src/custom-resources/topic-monitoring/topic-monitoring.go
+++ src/custom-resources/topic-monitoring/topic-monitoring.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-lambda-go/cfn"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 )
 
@@ -13,11 +14,27 @@ func main() {
 }
 
 func handler(ctx context.Context, event cfn.Event) (physicalResourceID string, data map[string]interface{}, err error) {
-	topicName := event.ResourceProperties["TopicName"].(string)
-	client := sns.NewFromConfig(*aws.NewConfig())
-	topic, _ := client.CreateTopic(ctx, &sns.CreateTopicInput{
-		Name: aws.String(topicName),
-	})
-	tData := map[string]interface{}{"Arn": topic.TopicArn}
-	return topicName, tData, nil
+	tData := map[string]interface{}{}
+	cfg, _ := config.LoadDefaultConfig(ctx)
+	if event.RequestType == "Create" {
+		topicName := event.ResourceProperties["TopicName"].(string)
+		client := sns.NewFromConfig(cfg)
+		topic, err := client.CreateTopic(ctx, &sns.CreateTopicInput{
+			Name: aws.String(topicName),
+		})
+		physicalResID := ""
+		if topic != nil {
+			physicalResID = aws.ToString(topic.TopicArn)
+		}
+		return physicalResID, tData, err
+	}
+	if event.RequestType == "Delete" {
+		topicArn := event.PhysicalResourceID
+		client := sns.NewFromConfig(cfg)
+		_, err = client.DeleteTopic(ctx, &sns.DeleteTopicInput{
+			TopicArn: aws.String(topicArn),
+		})
+		return topicArn, tData, err
+	}
+	return "", tData, nil
 }
